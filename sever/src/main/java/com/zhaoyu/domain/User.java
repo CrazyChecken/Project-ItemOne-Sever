674bba936ff42dd19f9d6baf974db4bdbd500c70
Zhaoyu <2905821826@qq.com>
2023-09-05 11:38:16 +0800

    配置id主键

--- sever/src/main/java/com/zhaoyu/domain/User.java
+++ sever/src/main/java/com/zhaoyu/domain/User.java
@@ -2,8 +2,16 @@ package com.zhaoyu.domain;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String name;
     private String gender;
