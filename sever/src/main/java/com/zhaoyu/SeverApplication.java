87033ca498013cfc4370425c079f77378426be58
Zhaoyu <2905821826@qq.com>
2023-09-05 10:10:45 +0800

    项目测试

--- sever/src/main/java/com/zhaoyu/SeverApplication.java
+++ sever/src/main/java/com/zhaoyu/SeverApplication.java
@@ -1,4 +1,4 @@
-package com.zhaoyu.sever;
+package com.zhaoyu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ sever/src/main/java/com/zhaoyu/web/BasisController.java
@@ -0,0 +1,12 @@
+package com.zhaoyu.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasisController {
+    @RequestMapping("/login")
+    public String login() {
+        return "这是登录页面";
+    }
+}
